b86ea4f9387fddb1ef4627543e612ae536cbc13c
Giovanni Bernardo <canaltamojuntooooo@gmail.com>
2024-12-11 15:17:19 -0300

    first commit

---
+++ src/main/java/acc/br/projetoQUATRO/ProjetoQuatroApplication.java
@@ -0,0 +1,13 @@
+package acc.br.projetoQUATRO;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoQuatroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoQuatroApplication.class, args);
+	}
+
+}
